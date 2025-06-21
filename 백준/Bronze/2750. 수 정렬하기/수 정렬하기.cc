af92f4e3531590540d22e10a4788e5c085e9a769
황정빈 <hjbin1211@gmail.com>
2025-06-21 20:18:58 +0900

    [Bronze II] Title: 수 정렬하기, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+
+    vector<int> arr(N);
+
+    for (int i = 0; i < N; ++i)
+        cin >> arr[i];
+
+    sort(arr.begin(), arr.begin() + N);
+
+    for (int i = 0; i < N; ++i)
+        cout << arr[i] << endl;
+}
